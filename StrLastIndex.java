4c794653a8f134b3aaecacba8d77139d9a67f685
apakde <apakde03@gmail.com>
2023-02-03 21:23:25 +0530

    Java Program

---
+++ StrLastIndex.java
@@ -0,0 +1,18 @@
+package Assignment_ARRAYS.Assignment;
+
+import java.util.Scanner;
+
+public class StrLastIndex {
+    public static void main(String[] args) {
+        Scanner n=new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String s1=n.nextLine();
+        int index=s1.lastIndexOf('a');
+        System.out.println(index);
+    }
+}
+//INPUT-
+//Enter a string:
+//java is a language
+//OUTPUT-
+//15
