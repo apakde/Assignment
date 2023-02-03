c887f79abad46ec03a244abe0c436cffd14587fb
apakde <apakde03@gmail.com>
2023-02-03 14:55:54 +0530

    Create StrCharat.java

---
+++ StrCharat.java
@@ -0,0 +1,9 @@
+package Assignment_ARRAYS.Assignment;
+
+public class StrCharat {
+    public static void main(String[] args) {
+        String example = "Hello Java";
+        char ch = example.charAt(6);
+        System.out.println(ch);
+    }
+}
