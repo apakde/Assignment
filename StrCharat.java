454646bf69527363170b38a99925720345fa472c
apakde <apakde03@gmail.com>
2023-02-03 15:16:30 +0530

    .

--- StrCharat.java
+++ StrCharat.java
@@ -1,9 +1,19 @@
 package Assignment_ARRAYS.Assignment;
 
+import java.util.Scanner;
+
 public class StrCharat {
     public static void main(String[] args) {
-        String example = "Hello Java";
-        char ch = example.charAt(6);
-        System.out.println(ch);
+        Scanner new1 = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String name = new1.nextLine();
+        char ch = name.charAt(4);
+        System.out.println("Character at 4 is:"+ch);
     }
 }
+
+//INPUT
+//Enter a string:
+//Hello Java
+//OUTPUT-
+//Character at 4 is:o
