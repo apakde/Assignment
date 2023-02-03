1032228a51b3b86f76ff676694b95da7d4adf6dc
apakde <apakde03@gmail.com>
2023-02-03 21:35:25 +0530

    Java Program

--- StrLength.java
+++ StrLength.java
@@ -1,4 +1,17 @@
 package Assignment_ARRAYS.Assignment;
 
+import java.util.Scanner;
+
 public class StrLength {
+    public static void main(String[] args) {
+        Scanner n1 = new Scanner(System.in);
+        System.out.println("Enter A string:");
+        String s1 = n1.nextLine();
+        System.out.println("Length of string is: "+s1.length());
+    }
 }
+//INPUT-
+//Enter A string:
+//Java is a programming lang.
+//OUTPUT-
+//Length of string is: 27
