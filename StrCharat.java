12a490aa778e32a6d830404430c72024bee5bf30
apakde <apakde03@gmail.com>
2023-02-03 17:40:38 +0530

    Java Program

--- StrCharat.java
+++ StrCharat.java
@@ -1,2 +1,18 @@
-package Assignment_ARRAYS.Assignment;public class StrCharAt {
+package Assignment_ARRAYS.Assignment;
+
+import java.util.Scanner;
+
+public class StrCharAt {
+    public static void main(String[] args) {
+        Scanner news = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String s1= news.nextLine();
+        char ch = s1.charAt(4);
+        System.out.println("Character at position 4 is:"+ch);
+    }
 }
+//INPUT-
+//Enter a string:
+//Hello Java
+//OUTPUT-
+//Character at position 4 is:o
