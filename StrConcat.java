e92e16b85d5b07b9d92c6b05d853abe91ecb29b7
apakde <apakde03@gmail.com>
2023-02-03 15:22:50 +0530

    .

--- StrConcat.java
+++ StrConcat.java
@@ -1,7 +1,21 @@
 package Assignment_ARRAYS.Assignment;
 
+import java.util.Scanner;
+
 public class StrConcat {
     public static void main(String[] args) {
-
+        Scanner new1 = new Scanner(System.in);
+        System.out.println("Enter two strings to concatenate:");
+        String s1= new1.nextLine();
+        String s2=new1.nextLine();
+        s1=s1.concat(s2);
+        System.out.println(s1);
     }
 }
+
+//INPUT-
+//Enter two strings to concatenate:
+//Sachin
+//Tendulkar
+//OUTPUT-
+//SachinTendulkar
