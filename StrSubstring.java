520221c7037cc453ef0fc93de93ec411ba50d2f2
apakde <apakde03@gmail.com>
2023-02-03 22:16:38 +0530

    Create StrSubstring.java

---
+++ StrSubstring.java
@@ -0,0 +1,17 @@
+package Assignment_ARRAYS.Assignment;
+
+import java.util.Scanner;
+
+public class StrSubstring {
+    public static void main(String[] args) {
+        Scanner neww = new Scanner(System.in);
+        System.out.println("Enter a string:");
+        String s1 = neww.nextLine();
+        System.out.println(s1.substring(2, 6));
+    }
+}
+//INPUT-
+//Enter a string:
+//javatpoint
+//OUTPUT-
+//vatp
