f6275f1ce9bcfbdfc355ab97361cc14316b07da6
apakde <apakde03@gmail.com>
2023-02-03 15:43:12 +0530

    Java Program

--- StrCharat.java
+++ StrCharat.java
@@ -1,19 +1,4 @@
 package Assignment_ARRAYS.Assignment;
 
-import java.util.Scanner;
-
-public class StrCharat {
-    public static void main(String[] args) {
-        Scanner new1 = new Scanner(System.in);
-        System.out.println("Enter a string:");
-        String name = new1.nextLine();
-        char ch = name.charAt(4);
-        System.out.println("Character at 4 is:"+ch);
-    }
+public class StrCharAt {
 }
-
-//INPUT
-//Enter a string:
-//Hello Java
-//OUTPUT-
-//Character at 4 is:o
---
+++ StrContains.java
@@ -0,0 +1,7 @@
+package Assignment_ARRAYS.Assignment;
+
+public class StrContains {
+    public static void main(String[] args) {
+
+    }
+}
